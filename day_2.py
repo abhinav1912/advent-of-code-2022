2a36ea8fb2309d495f49272619e77cba545e5bf0
abhinav1912 <abhinavmathur1912@gmail.com>
2022-12-04 23:17:11 +0530

    Day 2: completed part 2

--- day_2.py
+++ day_2.py
@@ -6,19 +6,21 @@ score = [
 shape = [1,2,3]
 map_1 = {"A": 0, "B": 1, "C": 2}
 map_2 = {"X": 0, "Y": 1, "Z": 2}
+part_2_map = [0,3,6]
 
 def convert(item):
     return (map_1[item[0]], map_2[item[1]])
 
+def shape_for_score(x, y):
+    for j in range(3):
+        if score[j][x] == part_2_map[y]:
+            return j
+
 with open("./day_2_input.txt", "r") as file: 
     data = [convert(x.strip('\n').split()) for x in file.readlines()]
     ans = 0
+    part_2 = 0
     for x, y in data:
         ans += score[y][x] + shape[y]
-    print(ans)
-    # for i in range(1, len(data)):
-    #     if data[i]:
-    #         data[i] += data[i-1]
-    # print(max(data))
-    # data.sort(reverse=True)
-    # print(sum(data[:3]))
+        part_2 += shape[shape_for_score(x, y)] + part_2_map[y]
+    print(part_2)

f9ba323fb5d91923c87123aa5c54a234386b86bd
abhinav1912 <abhinavmathur1912@gmail.com>
2022-12-08 22:18:45 +0530

    Day 5: part 1

--- day_5.py
+++ day_5.py
@@ -1,3 +1,22 @@
-with open("./day_5_input.txt", "r") as file:
-    data = [x.strip('\n') for x in file.readlines()]
-    print(data)
+import re
+
+dat = re.compile(r'\[(\w)\]')
+com = re.compile(r'move (\d+) from (\d+) to (\d+)')
+mem = [[] for i in range(9)]
+
+file = open("./day_5_input.txt", "r")
+for line in file.readlines():
+
+    for match in re.finditer(dat, line):
+            mem[match.start(1)//4].insert(0, match.group(1))
+    op = re.findall(com, line)
+    if op != []:
+            op = op[0]
+            for i in range(int(op[0])):
+                    mem[int(op[2])-1].append(mem[int(op[1])-1].pop())
+
+out = ""
+for i in range(9):
+    out += mem[i].pop()
+
+print(out)
